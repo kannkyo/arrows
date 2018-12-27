ae479f55c99fe24b0147d03db70da8a0d0719128
kannkyo <15080890+kannkyo@users.noreply.github.com>
2018-12-28 07:52:05 +0900

    Add volume backup tool

---
+++ _tools/backup/backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# reference
+# https://docs.docker.com/storage/volumes/#backup-restore-or-migrate-data-volumes
+
+backup_volume () {
+  docker run --rm --volumes-from $1 -v $(pwd)/backupdata:/backup ubuntu tar cvf /backup/$2.tar $3
+}
+
+# delete all tar
+rm backupdata/*.tar
+
+# backup
+while read row; do
+  volume_name=`echo ${row} | cut -d , -f 1`
+  tar_name=`echo ${row} | cut -d , -f 2`
+  directory_name=`echo ${row} | cut -d , -f 3`
+
+  echo backup_volume $volume_name $tar_name $directory_name
+       backup_volume $volume_name $tar_name $directory_name
+done < backupinfo.csv
+
