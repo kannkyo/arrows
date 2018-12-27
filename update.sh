3bad56af138626810d6c427aa1520633eb022d53
kannkyo <15080890+kannkyo@users.noreply.github.com>
2018-12-27 23:38:19 +0900

    Fix to change from docker pull to docker-compose pull

--- update.sh
+++ update.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-sudo docker pull gitlab/gitlab-ce
-sudo docker pull gitlab/gitlab-runner        
-sudo docker pull xetusoss/archiva            
-sudo docker pull sonatype/nexus3             
-sudo docker pull nginx                       
-sudo docker pull jenkins                     
-sudo docker pull postgres                    
-sudo docker pull redmine                     
-
+sudo docker-compose pull
