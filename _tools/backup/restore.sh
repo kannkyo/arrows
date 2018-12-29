dcf5de21786f863fc856e120e7a95202ae302081
kannkyo <15080890+kannkyo@users.noreply.github.com>
2018-12-29 23:14:02 +0900

    Add volume restore tool

---
+++ _tools/backup/restore.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# reference
+# https://docs.docker.com/storage/volumes/#backup-restore-or-migrate-data-volumes
+
+restore_volume () {
+  container=`echo $1 | cut -d , -f 1`
+  volume=`echo $1 | cut -d , -f 2`
+  directory=`echo $1 | cut -d , -f 3`
+
+
+  src=$(pwd)/backupdata
+  dst=/backup
+
+  set -x
+  docker stop $container
+  docker volume rm $volume
+  docker volume create $volume
+  docker run --rm --volumes-from $container -v $src:$dst ubuntu bash -c "cd ${directory} && tar zxf ${dst}/${volume}.tar.gz --strip 1"
+  set +x
+}
+
+# restore
+while read row; do
+  restore_volume ${row}
+done < backupinfo.csv
+
