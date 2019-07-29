fb9ac06976a7f3d4afecce82ad12e07552cb33d9
kannkyo <15080890+kannkyo@users.noreply.github.com>
2019-07-29 18:10:18 +0900

    Fix broken image #59

--- gitlab/ce/config/omnibus_config.rb
+++ gitlab/ce/config/omnibus_config.rb
@@ -1,5 +1,5 @@
 # gitlab
-external_url 'http://gitlab:80/gitlab'
+external_url 'http://localhost:80/gitlab'
 gitlab_rails['lfs_enabled'] = true
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')
 
